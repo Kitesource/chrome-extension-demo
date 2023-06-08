ee8e9168f54769538b007edc72b3fc746b3ad818
kite <2257440643@qq.com>
2023-06-08 14:32:11 +0800

    feat: inject js

--- src/content-scripts/content.ts
+++ src/content-scripts/content.ts
@@ -2,15 +2,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import '@/styles/index.scss'
 
-(() => {
-  const beforeRoot = document.querySelector('#crx-app')
-  if (beforeRoot && import.meta.env.MODE === 'development') {
-    document.body.removeChild(beforeRoot)
-  }
-  const el = document.querySelector('#rw-preload-landing-image_div')
-  if (el) {
-    el.insertAdjacentHTML('afterend', '<div id="crx-app"></div>')
-    const app = createApp(App)
-    app.mount('#crx-app')
-  }
-})()
+const script = document.createElement('script')
+script.src = chrome.runtime.getURL('public/injected.ts');
+(document.head || document.documentElement).appendChild(script)
+
+const beforeRoot = document.querySelector('#crx-app')
+if (beforeRoot && import.meta.env.MODE === 'development') {
+  document.body.removeChild(beforeRoot)
+}
+const el = document.querySelector('#rw-preload-landing-image_div')
+if (el) {
+  el.insertAdjacentHTML('afterend', '<div id="crx-app"></div>')
+  const app = createApp(App)
+  app.mount('#crx-app')
+}
